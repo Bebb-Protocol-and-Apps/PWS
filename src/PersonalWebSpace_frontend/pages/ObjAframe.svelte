84eff26326ffbae5af143fda357e47dfcd725d2e
patnorris <patrick@DESKTOP-TQ4V8LQ.localdomain>
2023-05-14 17:17:43 +0200

    Add code for mainnet experiments

--- src/PersonalWebSpace_frontend/pages/ObjAframe.svelte
+++ src/PersonalWebSpace_frontend/pages/ObjAframe.svelte
@@ -18,27 +18,27 @@
     //document.write(html);
     console.log("################################");
     // Define the user agent string for a popular browser.
-    const config = { 
+    /* const config = { 
       headers: { 
       'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/104.0.0.0 Safari/537.36', 
       }, 
     };
     const response = await axios.get('https://aframe.io/');
     console.log("response");
-    console.log(response);
-    /* const iframeElement = document.createElement('iframe');
-    iframeElement.src = 'http://172.25.153.17:3000/#/space/8';
+    console.log(response); */
+    const iframeElement = document.createElement('iframe');
+    //iframeElement.src = 'https://vdfyi-uaaaa-aaaai-acptq-cai.ic0.app/#/space/0';
     iframeElement.style.height = "200px";
-    iframeElement.style.width = "200px"; */
-    /* const url = "https://aframe.io/";
+    iframeElement.style.width = "200px";
+    const url = "https://vdfyi-uaaaa-aaaai-acptq-cai.ic0.app/#/space/0";
     let response = await fetch(url);
     console.log("response");
     console.log(response);
     const responseText = await response.text();
-    console.log(responseText); */
+    console.log(responseText);
     //let iframeElement = document.getElementById("html-fetch-iframe");
     //iframeElement.innerHTML = responseText;
-    //iframeElement.srcdoc = responseText;
+    iframeElement.srcdoc = responseText;
     //console.log("iframeElement");
     //console.log(iframeElement);
     
@@ -55,15 +55,16 @@
       const newDiv = document.createElement('div');
       newDiv.style.height = "200px";
       newDiv.style.width = "200px";
-      //newDiv.appendChild(iframeElement);
+      newDiv.appendChild(iframeElement);
       //newDiv.innerHTML = responseText;
-      newDiv.innerHTML = response.data;
+      //newDiv.innerHTML = response.data;
+      //newDiv.innerHTML = iframeElement.innerHTML;
       divIframe.appendChild(newDiv);
       const imgIframe = await html2canvas(divIframe);
       console.log("imgIframe");
       console.log(imgIframe);
-      divIframe.innerHTML = "";
-      divIframe.appendChild(imgIframe);
+      //divIframe.innerHTML = "";
+      //divIframe.appendChild(imgIframe);
       /* const imgEntity = html2canvas(document.getElementById("my-interface"));
       console.log("imgEntity");
       console.log(imgEntity);
