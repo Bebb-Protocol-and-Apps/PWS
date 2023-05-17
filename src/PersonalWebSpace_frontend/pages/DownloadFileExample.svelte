e84d3c24e212243d7652121f0fd800337e2bce84
Alexander Agudelo <awagud12@gmail.com>
2023-05-16 20:50:26 -0400

    Update downloading example

--- src/PersonalWebSpace_frontend/pages/DownloadFileExample.svelte
+++ src/PersonalWebSpace_frontend/pages/DownloadFileExample.svelte
@@ -16,7 +16,7 @@
       let fileIdsResponse = await $store.backendActor.listUserFileIds();
       let fileIds = fileIdsResponse.Ok.FileIds;
       console.log("Number of FileIds found: " + fileIds.length);
-      for (let i = 0; i < 1; i++)
+      for (let i = 0; i < fileIds.length; i++)
       {
         let fileData = await $store.backendActor.getFile(fileIds[i]);
 
@@ -26,31 +26,16 @@
         // Step 1: Convert the Uint8Array to a Blob
         const blob = new Blob([byteArray], { type: "application/octet-stream" });
 
-        // Step 2: Create a File object from the Blob
-        const fileName = fileData.Ok.File.file_name; // Replace with the actual file name
-        const lastModified = Date.now(); // Replace with the actual last modified timestamp if available
-
-        const file = new File([blob], fileName, { type: "application/octet-stream", lastModified });
-
-        // Now you have a File object
-        console.log(file);
-        const url = URL.createObjectURL(file, {type: "text/plain"});
-        console.log(url);
-
-        // Create an anchor element
-        const anchor = document.createElement('a');
-        anchor.href = url;
-        anchor.download = fileName; // Set the custom file name and extension
-        anchor.style.display = 'none';
-        document.body.appendChild(anchor);
-
-        const assetItem = document.getElementById('item2');
-        console.log(assetItem);
-        console.log(anchor);
-        console.log(`url(${url})`)
-        assetItem.setAttribute('gltf-model',`url(${url})`);
-        
-
+        // Step 2: Create a URL and pass it to the entities
+        const url = URL.createObjectURL(blob);
+        if (i == 0)
+        {
+          const assetItem = document.getElementById('item2');
+          assetItem.setAttribute('gltf-model',`url(${url})`);
+        } else {
+          const assetItem = document.getElementById('item3');
+          assetItem.setAttribute('gltf-model',`url(${url})`);
+        }
       }
     } catch (error) {
     console.error("Error:", error);
