a037f4fbc0199d6c259fb77193f5fb1b02cfcb9c
patnorris <patrick@DESKTOP-TQ4V8LQ.localdomain>
2023-09-04 18:35:35 +0200

    Add Exploration mode as stream of spaces

--- src/PersonalWebSpace_frontend/pages/ExploreSpaces.svelte
+++ src/PersonalWebSpace_frontend/pages/ExploreSpaces.svelte
@@ -1,17 +1,34 @@
 <script lang="ts">
-  import { onMount } from "svelte";
-  import { PersonalWebSpace_backend } from "canisters/PersonalWebSpace_backend";
+  import { onMount, afterUpdate } from "svelte";
   import { store } from "../store";
   
   import Topnav from "../components/Topnav.svelte";
   import Footer from "../components/Footer.svelte";
-  import UserSpaces from "../components/UserSpaces.svelte";
+  import UserSpace from "../components/UserSpace.svelte";
+
+  import { initiateCollapsibles } from "../helpers/space_helpers.js";
 
   import spinner from "../assets/loading.gif"; // TODO: load other assets (e.g. html pages) similarly (see https://vitejs.dev/guide/assets.html: Referenced assets are included as part of the build assets graph, will get hashed file names, and can be processed by plugins for optimization)
 
   const numberOfRandomSpacesToLoad = 3;
   let loading = true;
   let loadedRandomSpaces = [];
+  let currentIndex = 0;
+
+  const nextSpace = async () => {
+    if (currentIndex < loadedRandomSpaces.length - 2) {
+      currentIndex++;
+    } else {
+      currentIndex++;
+      await loadUserSpaces();
+    }
+  };
+
+  const prevSpace = () => {
+    if (currentIndex > 0) {
+      currentIndex--;
+    }
+  };
 
   const loadUserSpaces = async () => {
     let requestPromises = [];
@@ -27,24 +44,32 @@
         randomSpacesIds.push(spaceNFTResponses[j].Ok.id);
       };
     };
-    loadedRandomSpaces = randomSpaces;
+    loadedRandomSpaces = [...loadedRandomSpaces, ...randomSpaces];
     loading = false;
   };
 
   onMount(loadUserSpaces);
+  afterUpdate(initiateCollapsibles);
 </script>
 
 <Topnav />
 
-<section id="spaces" class="py-7 space-y-6 items-center text-center bg-slate-100">
+<section id="spaces" class="py-7 space-y-3 items-center text-center bg-slate-100">
   <h3 class="text-xl font-bold">Explore Web Spaces</h3>
   {#if loading}
     <p id='spacesSubtext'>Searching the Open Internet Metaverse for a few random Spaces to show you...</p>
     <img class="h-12 mx-auto" src={spinner} alt="loading animation" />
   {:else}
-    <p id='spacesSubtext'>These are a few Spaces that OIM Users own:</p>
-    <div id='randomSpaces' class="space-y-4">
-      <UserSpaces spaces={loadedRandomSpaces} />
+    <div id='randomSpaces' class="space-y-1">
+      {#if currentIndex > 0}
+        <button on:click={prevSpace} class="active-app-button bg-slate-500 text-white py-2 px-4 rounded font-semibold">Previous</button>
+      {:else}
+        <button disabled class="bg-slate-500 text-white py-2 px-4 rounded font-bold opacity-50 cursor-not-allowed">Previous</button>
+      {/if}
+      <button on:click={nextSpace} class="active-app-button bg-slate-500 text-white py-2 px-4 rounded font-semibold">Next</button>
+      {#key currentIndex}
+        <UserSpace space={loadedRandomSpaces[currentIndex]} spaceIframeHeight={"500px"}/>
+      {/key}
     </div>
   {/if}
 </section>
